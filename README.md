50e25a5aaf84fbc359ef25efe959faa62732f423
masanao <mhamada01@gmail.com>
2026-05-10 20:41:55 +0900

    リファクタ

---
+++ README.md
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d '{"mail": "test@test.com", "password": "password"}' "http://localhost:8080/examcard/login"
--- src/main/java/com/examcard/controller/LoginContoller.java
+++ src/main/java/com/examcard/controller/LoginContoller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class LoginContoller {
 		this.loginService = loginService;
 	}
 
-	@GetMapping(value = {"/login"})
+	@PostMapping(value = {"/login"})
 	public LoginContollerOutputDto index(@RequestBody @Validated LoginContollerInputDto loginContollerInputDto, BindingResult result) {
 		if (result.hasErrors()) {
 			throw new BusinessException(ErrorCode.W400000, result.getAllErrors(), this.getClass().getName());
